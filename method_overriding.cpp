0d042b12f40b4cbb1cbb2d48f4d24dc126559152
rashan <40189953+rashanjotkaur@users.noreply.github.com>
2020-07-23 22:31:33 -0700

    Update method_overriding.cpp

--- method_overriding.cpp
+++ method_overriding.cpp
@@ -1,3 +1,38 @@
+// Before Overriding
+#include <bits/stdc++.h>
+using namespace std;
+class BaseClass{
+    public:
+        void show(){
+            cout<<"Base Class"<<endl;
+        }
+};
+class DerivedClass : public BaseClass{
+    public:
+        void show(){
+            cout<<"Derived Class"<<endl;
+        }
+};
+int main(){
+    BaseClass *obj1;
+    obj1 = new DerivedClass();
+    obj1->show();
+    
+    BaseClass *obj2;
+    obj2 = new BaseClass();
+    obj2->show();
+    
+    DerivedClass *obj3;
+    obj3 = new DerivedClass();
+    obj3->show();
+    return 0;
+}
+// Output:
+// Base Class
+// Base Class
+// Derived Class
+
+// After Overriding
 #include <bits/stdc++.h>
 using namespace std;
 class BaseClass{
@@ -26,3 +61,7 @@ int main(){
     obj3->show();
     return 0;
 }
+// Output:
+// Derived Class
+// Base Class
+// Derived Class
