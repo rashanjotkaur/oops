5797067e6df4ce9387a097893216243deb5ead23
rashan <40189953+rashanjotkaur@users.noreply.github.com>
2020-07-23 22:28:38 -0700

    Create method_overriding.cpp

---
+++ method_overriding.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class BaseClass{
+    public:
+        virtual void show(){
+            cout<<"Base Class"<<endl;
+        }
+};
+class DerivedClass : public BaseClass{
+    public:
+        void show() override{
+            cout<<"Derived Class"<<endl;
+        }
+};
+int main(){
+    BaseClass *obj1;
+    obj1 = new DerivedClass();
+    obj1->show();
+    
+    BaseClass *obj2;
+    obj2 = new BaseClass();
+    obj2->show();
+    
+    DerivedClass *obj3;
+    obj3 = new DerivedClass();
+    obj3->show();
+    return 0;
+}
